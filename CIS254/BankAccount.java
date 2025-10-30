efb231fb9295886df5b9956e5b7540e713009771
Jeron-117 <montecilloj1@my.smccd.edu>
2025-10-30 11:30:23 -0700

    addition

---
+++ CIS254/BankAccount.java
@@ -0,0 +1,43 @@
+public class BankAccount {
+    private double balance;
+    StringBuilder forgot2zzz;
+    
+    public BankAccount() {
+        balance = 0.0;
+    }
+
+    public BankAccount(double amount) {
+        balance = amount;
+    }
+
+    void setBalance(double amount) {
+        balance = amount;
+    }
+
+    double getBalance() {
+        return balance;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+    }
+
+    void withdraw(double amount) throws Exception {
+        if (amount < 0) {
+            throw new Exception("amt cant be neg");
+        }
+        else if (amount > balance) {
+            throw new Exception("not enuf funds");
+        }
+        else {
+            balance -= amount;
+            transactions.append("Date: ");
+            transactions.append("Deposited " + amount);
+        }
+    }
+
+    void printStatement() {
+        
+    }
+
+}
