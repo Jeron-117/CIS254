c88563dec23b09957e09c7dc8a557bfac6a071aa
Jeron-117 <montecilloj1@my.smccd.edu>
2025-11-04 11:20:04 -0800

    13

---
+++ CIS254/HW13/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("scrab scrab");
+        Tile tileJ = new Tile('J', 8);
+        System.out.println("Tile: " + tileJ);
+    }
+
+}
