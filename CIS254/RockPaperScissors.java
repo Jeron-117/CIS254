75558064975c0fc54fdfc71afd2528ea3eaf47ea
Jeron-117 <montecilloj1@my.smccd.edu>
2025-12-02 10:54:05 -0800

    Update RockPaperScissors.java

--- CIS254/RockPaperScissors.java
+++ CIS254/RockPaperScissors.java
@@ -1,5 +1,10 @@
-public static class RockPaperScissors {
-    public static boolean JoelOdessky(String y) {
-        return y;
+public class App {
+    public static void main(String[] args) throws exception {
+        MainFrame myFrame = new MainFrame();
+        myFrame.initialize();
+
+
+
+
     }
 }
